b6451a7e836dbad929483582e26795b5e14ceca4
KishanThorat111 <KishanThorat111@gmail.com>
2025-03-19 22:17:45 +0000

    Updated login page and user service

--- foodcorner/src/app/services/user.service.ts
+++ foodcorner/src/app/services/user.service.ts
@@ -1,65 +1,65 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { IUserLogin } from '../shared/interfaces/IUserLogin';
-import { HttpClient } from '@angular/common/http';
-import { USER_LOGIN_URL } from '../shared/constants/url';
-import { ToastrService } from 'ngx-toastr';
-import { User } from '../shared/model/Users';
-import { Router } from '@angular/router';
+// import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+// import { BehaviorSubject, Observable, tap } from 'rxjs';
+// import { IUserLogin } from '../shared/interfaces/IUserLogin';
+// import { HttpClient } from '@angular/common/http';
+// import { USER_LOGIN_URL } from '../shared/constants/url';
+// import { ToastrService } from 'ngx-toastr';
+// import { User } from '../shared/model/Users';
+// import { Router } from '@angular/router';
 
-const USER_KEY = 'User'
-@Injectable({
-  providedIn: 'root'
-})
+// const USER_KEY = 'User'
+// @Injectable({
+//   providedIn: 'root'
+// })
 
-export class UserService {
+// export class UserService {
 
-  private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage());
-  public userObservable: Observable<User>;
+//   private userSubject = new BehaviorSubject<User>(this.getUserFromLocalStorage());
+//   public userObservable: Observable<User>;
 
-  constructor(
-    private http: HttpClient, 
-    private toastrService: ToastrService,
-    private router: Router
-  ) { 
-    this.userObservable = this.userSubject.asObservable();
-  }
+//   constructor(
+//     private http: HttpClient, 
+//     private toastrService: ToastrService,
+//     private router: Router
+//   ) { 
+//     this.userObservable = this.userSubject.asObservable();
+//   }
 
-  login(userLogin: IUserLogin): Observable<User> {
-    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
-      tap({
-        next: (user) => {
-          this.setUserToLocalStorage(user);
-          this.userSubject.next(user);
-          this.toastrService.success(`Welcome to KT Foodcorner ${user.name}!`, 'Login Successful!');
-        },
-        error: (errorResponse) => {
-          this.toastrService.error(errorResponse.error, 'Login Failed!');
-        }
-      })
-    );
-  }
+//   login(userLogin: IUserLogin): Observable<User> {
+//     return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+//       tap({
+//         next: (user) => {
+//           this.setUserToLocalStorage(user);
+//           this.userSubject.next(user);
+//           this.toastrService.success(`Welcome to KT Foodcorner ${user.name}!`, 'Login Successful!');
+//         },
+//         error: (errorResponse) => {
+//           this.toastrService.error(errorResponse.error, 'Login Failed!');
+//         }
+//       })
+//     );
+//   }
 
-  logout(): void {
-    this.userSubject.next(new User());
-    localStorage.removeItem(USER_KEY);
-    this.router.navigate(['/login']);
-  }
+//   logout(): void {
+//     this.userSubject.next(new User());
+//     localStorage.removeItem(USER_KEY);
+//     this.router.navigate(['/login']);
+//   }
 
-  private setUserToLocalStorage(user: User): void {
-    localStorage.setItem(USER_KEY, JSON.stringify(user));
-  }
+//   private setUserToLocalStorage(user: User): void {
+//     localStorage.setItem(USER_KEY, JSON.stringify(user));
+//   }
 
-  private getUserFromLocalStorage(): User {
-    try {
-      const userJson = localStorage.getItem(USER_KEY);
-      return userJson ? JSON.parse(userJson) as User : new User();
-    } catch (error) {
-      console.error('Error parsing user data from localStorage', error);
-      return new User();
-    }
-  }
-}
+//   private getUserFromLocalStorage(): User {
+//     try {
+//       const userJson = localStorage.getItem(USER_KEY);
+//       return userJson ? JSON.parse(userJson) as User : new User();
+//     } catch (error) {
+//       console.error('Error parsing user data from localStorage', error);
+//       return new User();
+//     }
+//   }
+// }
 
 
 
@@ -99,3 +99,81 @@ export class UserService {
 //   return new User();
 //   }
 // }
+
+
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/url';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../shared/model/Users';
+import { Router } from '@angular/router';
+
+const USER_KEY = 'User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable: Observable<User>;
+
+  constructor(
+    private http: HttpClient, 
+    private toastrService: ToastrService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: object // Inject PLATFORM_ID to detect environment
+  ) { 
+    if (isPlatformBrowser(this.platformId)) {
+      const user = this.getUserFromLocalStorage();
+      this.userSubject.next(user);
+    }
+    this.userObservable = this.userSubject.asObservable();
+  }
+
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+          if (isPlatformBrowser(this.platformId)) {
+            this.setUserToLocalStorage(user);
+          }
+          this.userSubject.next(user);
+          this.toastrService.success(`Welcome to KT Foodcorner ${user.name}!`, 'Login Successful!');
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Login Failed!');
+        }
+      })
+    );
+  }
+
+  logout(): void {
+    this.userSubject.next(new User());
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(USER_KEY);
+    }
+    this.router.navigate(['/login']);
+  }
+
+  private setUserToLocalStorage(user: User): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(USER_KEY, JSON.stringify(user));
+    }
+  }
+
+  private getUserFromLocalStorage(): User {
+    if (isPlatformBrowser(this.platformId)) {
+      try {
+        const userJson = localStorage.getItem(USER_KEY);
+        return userJson ? JSON.parse(userJson) as User : new User();
+      } catch (error) {
+        console.error('Error parsing user data from localStorage', error);
+        return new User();
+      }
+    }
+    return new User();
+  }
+}
